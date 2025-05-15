6d2c575aa9300a980d3587bad855e6303f0e1a51
32comma <louis.develog@gmail.com>
2025-05-15 12:59:43 +0900

    update

---
+++ 028_객체_이해하기/main.js
@@ -0,0 +1,17 @@
+var family = {
+    adress: "Seoul",
+    members: {},
+    addFamily: (age, name, role) => {
+        this.members[role] = {
+            age: age,
+            name: name
+        }
+    },
+    getHeadcount: function () {
+        return Object.keys(this.members).length;
+    }
+}
+family.addFamily(30, 'chloe', 'aunt');
+family.addFamily(3, 'lyn', 'niece');
+family.addFamily(10, 'dangdanggi', 'dog');
+console.log(family.getHeadcount());
