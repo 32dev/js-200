790250e1ce6f1cb32c91142542676e66faa93e8d
dev-32 <louis.develog@gmail.com>
2025-05-30 13:02:39 +0900

    function

---
+++ 034_함수_이해하기/main.js
@@ -0,0 +1,8 @@
+var greeting_expression = function (name) {
+    console.log("Hi, " + name)
+}
+function greeting_declaration(name) {
+    console.log("Hi, " + name)
+}
+greeting_declaration('Chloe');
+greeting_expression('Chloe');
