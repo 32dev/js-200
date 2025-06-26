7b7bebb2f518979c89b508a5c149222ff5573181
32comma <louis.develog@gmail.com>
2025-06-27 08:18:07 +0900

    update

---
+++ 02_중급/064_배열_자료형_확인하기/064.js
@@ -0,0 +1,8 @@
+function callRoll(students) {
+    if (!Array.isArray(students)) return;
+    students.forEach((studnet) =>){
+        console.log(`$Are you here, ${student}`);;
+    }
+}
+const students = ['Jun', 'Ali', 'Murry', 'Toby'];;
+callRoll(students);
