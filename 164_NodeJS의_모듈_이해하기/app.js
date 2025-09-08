d3345188788a22c8a09d99c7f2d549e0e78ade61
32dev <louis.develog@gmail.com>
2025-09-08 12:48:52 +0900

    update

---
+++ 164_NodeJS의_모듈_이해하기/app.js
@@ -0,0 +1,2 @@
+const greet = require('./greet.js');
+greet('Javascript 200');
---
+++ 164_NodeJS의_모듈_이해하기/greet.js
@@ -0,0 +1,5 @@
+console.log('Hello');
+const greet = function (name) {
+    console.log('How are you?' + name);
+}
+module.exports = greet;
