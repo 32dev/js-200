f2f74e0bf976132dc783bdb2c8b56ca16d2bb820
32dev <louis.develog@gmail.com>
2025-09-08 12:30:00 +0900

    update

--- 161_웹_워커로_백그라운드_코드_실행하기/fibonacci.js
+++ 161_웹_워커로_백그라운드_코드_실행하기/fibonacci.js
@@ -5,10 +5,10 @@ function fibonacci(num) {
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-onmessage = function(e) {
+onmessage = function (e) {
   const num = e.data.num;
   console.log('메인 스크립트에서 전달 받은 메시지', e.data);
-  if (num == null || num === "")  {
+  if (num == null || num === "") {
     throw new Error('숫자를 전달하지 않았습니다.');
   }
   const result = fibonacci(num);
