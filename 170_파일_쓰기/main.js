0b294877da115453a705af3ae022fe3a87c9659d
32dev <louis.develog@gmail.com>
2025-09-08 13:14:26 +0900

    170

--- 170_파일_쓰기/main.js
+++ 170_파일_쓰기/main.js
@@ -5,9 +5,7 @@ const path = require('path');
 
 const makeFile = (filePath, callback) => {
     fs.writeFile(filePath, 'New file, New content', 'utf8', (err) => {
-        if (err) {
-            return callback(err);
-        }
+        if (err) return callback(err);
         console.log('파일이 생성됐습니다.');
         callback(null);
     });
@@ -15,30 +13,24 @@ const makeFile = (filePath, callback) => {
 
 const appendFile = (filePath, callback) => {
     fs.appendFile(filePath, '\nUpdate file', (err) => {
-        if (err) {
-            return callback(err);
-        }
+        if (err) return callback(err);
         console.log('파일 내용을 추가합니다.');
         callback(null);
     });
 };
 
 const printErrIfExist = (err) => {
-    if (err) {
-        console.error(err);
-    }
+    if (err) console.error(err);
 };
 
 const filePath = path.join(__dirname, 'js200', 'hello.txt');
 
-fs.open(filePath, 'wx', (err, fd) => {
-    if (err && err.code === 'EEXIST') {
-        // 파일이 이미 있으면 내용 추가
-        return appendFile(filePath, (err) => printErrIfExist(err));
-    }
+// 파일 존재 여부 확인
+fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
-        return printErrIfExist(err);
+        // 파일 없음 → 새 파일 생성
+        return makeFile(filePath, printErrIfExist);
     }
-    // 새 파일 생성
-    return makeFile(filePath, (err) => printErrIfExist(err));
+    // 파일 있음 → 내용 추가
+    return appendFile(filePath, printErrIfExist);
 });
