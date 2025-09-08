63e02ff9117737faf6a2b95fc2164910d7ea89b8
32dev <louis.develog@gmail.com>
2025-09-08 12:58:40 +0900

    168

---
+++ 168_event_emitter2/eventEmitter.js
@@ -0,0 +1,12 @@
+const Emitter = require('events');
+const eventConfig = require('./config').events;
+const em = new Emitter();
+em.on(eventConfig.GREET, () => {
+    console.log('Somewhere, someone said hello');
+});
+
+em.on(eventConfig.GREET, () => {
+    console.log('A greeting occurred!');
+});
+
+em.emit(eventConfig.GREET);
